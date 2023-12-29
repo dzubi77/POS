b9f31ae55b56e83b5d9e0b576e2c4b2145c13bc1
dzubi77 <mat.dzubak@gmail.com>
2023-12-29 13:08:24 +0100

    init commit

---
+++ cmake-build-debug/CMakeFiles/POS_SP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS_SP.dir/main.c.o"
+  "POS_SP"
+  "POS_SP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POS_SP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
